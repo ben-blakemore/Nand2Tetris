9dffa992c66866312cacd326f53599a3bd50b77b
Ben Blakemore <blakemoreb1000@gmail.com>
2023-03-20 15:29:23 +0000

    Work on assembler

--- nand2tetris/projects/06/assembler.py
+++ nand2tetris/projects/06/assembler.py
@@ -1,6 +1,6 @@
 import sys
 
-comp_table ={
+comp_table = {
     "0":  "0101010",
     "1":  "0111111",
     "-1": "0111010",
@@ -31,16 +31,54 @@ comp_table ={
 }
 
 dest_table = {
-    "": "000",
-    
+    "":   "000",
+    "M":  "001",
+    "D":  "010",
+    "MD": "011",
+    "A":  "100",
+    "AM": "101",
+    "AD": "110",
+    "AMD":"111"
 }
 
-def parse_file(file):
+jump_table = {
+    "":   "000",
+    "JGT":"001",
+    "JEQ":"010",
+    "JGE":"011",
+    "JLT":"100",
+    "JNE":"101",
+    "JLE":"110",
+    "JMP":"111"
+}
+
+command_list = []
+
+def parse_a_instruction(instruction):
+    register = int(instruction.rsplit("@")[1]) # Get register number to convert to binary
+    register_binary = format(register, 'b').zfill(15)
+    command_list.append("0" + register_binary)
+
+def parse_c_instruction(instruction):
+    instruction_split = instruction.split("=")
+    dest = instruction_split[0]
+    comp = instruction_split[1]
+    binary = "111"
+    return "binary"
+
+def parse_file(input_file):
+    for line in input_file.readlines():
+        if line[0] == "/" or line[0] == "\n": # Is a comment
+            continue
+        if line[0] == "@":
+            parse_a_instruction(line.strip())
+        else:
+            parse_c_instruction(line.strip())
 
 if __name__ == "__main__":
-    input_file = sys.argv[1]
-    if input_file is None:
+    file = sys.argv[1]
+    if file is None:
         print("Must pass in a .asm input file")
-    initialise_symbol_table()
     # parse the file
-    parse_file(input_file)
+    with open(file, 'r') as input_file:
+        result = parse_file(input_file)
