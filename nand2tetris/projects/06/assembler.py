4e5f4b418177086b35ea0eb3db189d1d775c1eaf
Ben Blakemore <blakemoreb1000@gmail.com>
2023-03-22 21:04:34 +0000

    Started work on symbol recognition

--- nand2tetris/projects/06/assembler.py
+++ nand2tetris/projects/06/assembler.py
@@ -27,7 +27,8 @@ comp_table = {
     "M-D":"1000111",
     "D&A":"0000000",
     "D&M":"1000000",
-    "D|A":"0010101"
+    "D|A":"0010101",
+    "D|M":"1010101"
 }
 
 dest_table = {
@@ -53,10 +54,26 @@ jump_table = {
 }
 
 command_list = []
+symbol_table = {}
+symbol_count = 0
+
+def convert_to_binary(number):
+    return format(int(number), 'b').zfill(16)
 
 def parse_a_instruction(instruction):
-    register = int(instruction.rsplit("@")[1]) # Get register number to convert to binary
-    register_binary = format(register, 'b').zfill(15)
+    symbol = instruction.split("@")[1] # @R0 -> R0 in order to look up in symbol table
+    if symbol in symbol_table:
+        command_list.append(convert_to_binary(symbol_table[symbol]))
+        return
+    # Not in symbol table so either @3 or @i symbol. If @i then want to add to symbol table
+    if not symbol.isnumeric():
+        global symbol_count
+        symbol_table[symbol] = symbol_count + 16
+        # command_list.append(convert_to_binary(symbol_table[symbol]))
+        symbol_count += 1
+        return
+    register = int(instruction.rsplit("@")[1]) # Get register number to convert to binary#
+    register_binary = convert_to_binary(register)
     command_list.append("0" + register_binary)
 
 def parse_c_instruction(instruction):
@@ -64,6 +81,8 @@ def parse_c_instruction(instruction):
     jump_next = False
 
     for char in instruction:
+        if char == " ":
+            break # Some lines have a C instruction and then comment, we want to ignore that
         if jump_next:
             jump_command += char
         elif char == "=":
@@ -91,14 +110,36 @@ def parse_c_instruction(instruction):
 
 def parse_file(input_file):
     for line in input_file.readlines():
-        if line[0] == "/" or line[0] == "\n": # Is a comment
+        line = line.strip()
+        if line == "" or line[0] == "/": # Is a comment
             continue
-        if line[0] == "@":
-            parse_a_instruction(line.strip())
+        elif line[0] == "(": # Is a symbol
+            symbol_table[line] = len(command_list) + 1
+        elif line[0] == "@":
+            parse_a_instruction(line)
         else:
-            parse_c_instruction(line.strip())
+            parse_c_instruction(line)
+
+def initialise_symbol_table():
+    # Initialise symbol table with predefined symbols
+    symbol_table = {
+        "SCREEN": 16384,
+        "KBD": 24576,
+        "SP": 0,
+        "LCL": 1,
+        "ARG": 2,
+        "THIS":3,
+        "THAT":4
+    }
+
+    for i in range(16):
+        symbol_table[f"R{i}"] = i
+    
+    return symbol_table
 
 if __name__ == "__main__":
+    symbol_table = initialise_symbol_table()
+
     file = sys.argv[1]
     if file is None:
         print("Must pass in a .asm input file")
@@ -108,3 +149,4 @@ if __name__ == "__main__":
     with open(f'{file.split(".")[0]}.hack', 'w') as output_file:
         for line in command_list:
             output_file.write(line + "\n")
+    print(symbol_table)
